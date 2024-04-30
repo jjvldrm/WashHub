46e9f8bf0fe04ba1bfeae0901df68e68a02cd116
jjvldrm <johnjoseph.valderama.f@bulsu.edu.ph>
2024-04-30 13:41:43 +0800

    Initial Page Change

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Services from './components/Services/Services';
-import Book from './components/Book/Book';
-import Reviews from './components/Reviews/Reviews';
+import React from 'react';
+import LandingPage from './pages/LandingPage';
+import Header from './components/Header.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/book" element={<Book />} />
-          <Route path="/reviews" element={<Reviews />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className='bg-primary h-100 bg-with-image'>
+      <Header />
+      <LandingPage />
+    </div>
   );
 }
-
-export default App;
